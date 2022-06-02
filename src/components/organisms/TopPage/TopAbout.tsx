93224f1b8466db602aea0202446a0e8dc817779e
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-03 00:35:26 +0900

    topページのABOUT部分を実装

---
+++ src/components/organisms/TopPage/TopAbout.tsx
@@ -0,0 +1,30 @@
+export const TopAbout = () => {
+	return (
+		<>
+			<div className="top-about">
+				<div className="top-about__content-wrap">
+					<div className="top-about__heading">
+						<h1 className="top-about__title">ABOUT</h1>
+						<p className="top-about__subtitle">芸工祭とは</p>
+					</div>
+					<div className="top-about__description">
+						<p className="top-about__description-text">
+							装飾に彩られたキャンパス<br/>
+							個性豊かな作品の展示<br/>
+							笑いの溢れるステージ企画に<br/>
+							アイデアたっぷりの模擬店。<br/>
+							極め付けはオシャレで美味しい本格カフェ。<br/>
+							<br/>
+							とにかく見どころ満載で<br/>
+							妥協のないこの学祭は<br/>
+							<span>芸工生全員</span>で作る一つの作品です！
+						</p>
+						<h2 className="top-about__description-main">
+							みんなで盛り上げよう
+						</h2>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,12 +1,14 @@
 import { memo } from "react";
 
-import { TopVisual } from "../organisms/TopVisual";
+import { TopVisual } from "../organisms/TopPage/TopVisual";
+import { TopAbout } from "../organisms/TopPage/TopAbout";
 import "../../styles/top.scss";
 
 export const TopPage = memo(() => {
 	return (
 		<>
 			<TopVisual />
+			<TopAbout />
 		</>
 	);
 })
