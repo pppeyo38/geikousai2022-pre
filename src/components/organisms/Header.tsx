37f257a665b77e9be2931fa5ac6b29f8ee54ac12
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-08 22:52:23 +0900

    navメニューの出現をフェードっぽく実装

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,14 +1,18 @@
-import { memo, useState} from "react";
+import { memo } from "react";
+import { Fade, useDisclosure } from "@chakra-ui/react";
+import { animated } from "react-spring";
+
+// import Burger from "@animated-burgers/burger-squeeze";
+// import '@animated-burgers/burger-squeeze/dist/styles.css';
 
 import { SnsIcons } from "../atoms/SnsIcons";
 
 import "../../styles/base/header.scss";
 
 export const Header = memo(() => {
-	const [ isOpen, setIsOpen ] = useState(false);
-	const onClickTaggle = () => {
-		setIsOpen(!isOpen);
-	}
+	const { isOpen, onToggle } = useDisclosure();
+
+
 
 	return (
 		<>
@@ -18,57 +22,56 @@ export const Header = memo(() => {
 						<img className="Header-logo" src={`${process.env.PUBLIC_URL}/icons/HeaderLogo.svg`} alt="芸工祭ロゴ画像"/>
 						<img className="Header-title" src={`${process.env.PUBLIC_URL}/icons/HeaderTitle.svg`} alt="芸工祭タイトル画像"/>
 					</a>
-					<div className="Header-menu-icon" onClick={onClickTaggle}>
+					{/* <animated.div className="Header-menu-icon" onClick={onToggle} style={}>
 						<span></span>
+					</animated.div> */}
+					<div className="burger burger-squeeze">
+  					<div className="burger-lines"></div>
 					</div>
+					<Fade in={isOpen} className="nav__overlay">
+						{/* <div className="nav__overlay-close-icon">
+							<div className="nav__overlay-close-icon-wrap" onClick={onToggle}>
+								<span></span>
+							</div>
+						</div> */}
+						<ul className="nav__overlay-menuList">
+							<li>
+								<a href="/">
+									<h3>TOP</h3>
+									<p>トップ</p>
+								</a>
+							</li>
+							<li>
+								<a href="/departments">
+									<h3>DEPARTMENTS</h3>
+									<p>部署紹介</p>
+								</a>
+							</li>
+							<li>
+								<a href="/chart">
+									<h3>CHART</h3>
+									<p>チャート</p>
+								</a>
+							</li>
+							<li>
+								<a href="/movie">
+									<h3>MOVIE</h3>
+									<p>ムービー</p>
+								</a>
+							</li>
+							<li>
+								<a href="/game">
+									<h3>GAME</h3>
+									<p>ゲーム</p>
+								</a>
+							</li>
+						</ul>
+						<div className="nav__overlay-sns-wrap">
+							<SnsIcons />
+						</div>
+					</Fade>
 				</div>
 			</header>
-
-			{isOpen &&
-				<nav className="nav__overlay">
-					<div className="nav__overlay-close-icon" onClick={onClickTaggle}>
-						<div className="nav__overlay-close-icon-wrap">
-							<span></span>
-						</div>
-					</div>
-					<ul className="nav__overlay-menuList">
-						<li>
-							<a href="/">
-								<h3>TOP</h3>
-								<p>トップ</p>
-							</a>
-						</li>
-						<li>
-							<a href="/departments">
-								<h3>DEPARTMENTS</h3>
-								<p>部署紹介</p>
-							</a>
-						</li>
-						<li>
-							<a href="/chart">
-								<h3>CHART</h3>
-								<p>チャート</p>
-							</a>
-						</li>
-						<li>
-							<a href="/movie">
-								<h3>MOVIE</h3>
-								<p>ムービー</p>
-							</a>
-						</li>
-						<li>
-							<a href="/game">
-								<h3>GAME</h3>
-								<p>ゲーム</p>
-							</a>
-						</li>
-					</ul>
-					<div className="nav__overlay-sns-wrap">
-						<SnsIcons />
-					</div>
-				</nav>
-			}
-
 		</>
 	);
 })
