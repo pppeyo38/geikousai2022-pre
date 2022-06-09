89f0d9f8da2f04b5e26946fd71445e3364cdecd2
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-09 23:26:54 +0900

    ムービーページのタイトル作成

--- src/components/pages/MoviePage.tsx
+++ src/components/pages/MoviePage.tsx
@@ -1,7 +1,23 @@
+import { Heading } from "../atoms/Heading";
+
+import "../../styles/movie.scss";
+
 export const MoviePage = () => {
 	return (
 		<>
-			<h1>ムービーページ</h1>
+			<div className="p-movie">
+				<div className="p-movie__heading">
+					<h1 className="p-movie__title">MOVIE</h1>
+					<p className="p-movie__subtitle">ムービー</p>
+				</div>
+				<h3 className="p-moive__about">
+					映像部署が制作したムービーです。
+				</h3>
+
+				<div className="p-movie__concept-wrap">
+					<Heading>コンセプトムービー</Heading>
+				</div>
+			</div>
 		</>
 	);
 }
