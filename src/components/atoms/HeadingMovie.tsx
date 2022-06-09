d3e28503d20388b8be31c837a6c1d1974f2f6a37
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-10 01:16:19 +0900

    movieページの作成

---
+++ src/components/atoms/HeadingMovie.tsx
@@ -0,0 +1,19 @@
+import { FC, memo, ReactNode } from "react";
+
+import "../../styles/modules/heading.scss";
+
+type Props = {
+	children: ReactNode;
+};
+
+export const HeadingMovie: FC<Props> = memo((props) => {
+	const { children } = props;
+
+	return (
+		<div className="HeadingMovie-wrap" id="HeadingMovieBox">
+			<div className="HeadingMovieBlackRect mv__top-left"></div><div className="HeadingMovieBlackRect mv__top-rigt"></div><div className="HeadingMovieBlackRect mv__btm-left"></div><div className="HeadingMovieBlackRect mv__btm-rigt"></div>
+			<div className="HeadingMovieText">{children}</div>
+		</div>
+	);
+
+})
--- src/components/pages/MoviePage.tsx
+++ src/components/pages/MoviePage.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "../atoms/Heading";
+import { HeadingMovie } from "../atoms/HeadingMovie";
 
 import "../../styles/movie.scss";
 
@@ -10,12 +10,18 @@ export const MoviePage = () => {
 					<h1 className="p-movie__title">MOVIE</h1>
 					<p className="p-movie__subtitle">ムービー</p>
 				</div>
-				<h3 className="p-moive__about">
+				<h3 className="p-movie__about">
 					映像部署が制作したムービーです。
 				</h3>
 
-				<div className="p-movie__concept-wrap">
-					<Heading>コンセプトムービー</Heading>
+				<div className="p-movie__concept">
+					<HeadingMovie>コンセプトムービー</HeadingMovie>
+					<div className="p-movie__concept-inner"></div>
+				</div>
+
+				<div className="p-movie__busyocho">
+					<HeadingMovie>部署長紹介</HeadingMovie>
+					<div className="p-movie__busyocho-inner"></div>
 				</div>
 			</div>
 		</>
