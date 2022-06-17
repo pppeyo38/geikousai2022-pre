c27d251e2aab2e2b98449b01e47be8c8b4dacf0b
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-17 11:26:25 +0900

    react-unity-webglの導入

--- src/components/pages/GamePage.tsx
+++ src/components/pages/GamePage.tsx
@@ -1,7 +1,20 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 export const GamePage = () => {
+	// const { unityProvider } = useUnityContext({
+	// 	loaderUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.loader.js`,
+	// 	dataUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.data`,
+	// 	frameworkUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.framework.js.gz`,
+	// 	codeUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.wasm.gz`,
+	// 	webglContextAttributes: {
+  //     preserveDrawingBuffer: true,
+  //   },
+	// });
+
 	return (
 		<>
-			<h1>ミニゲームページ</h1>
+			{/* <Unity unityProvider={unityProvider} /> */}
+			<p>ミニゲーム</p>
 		</>
 	);
 }
