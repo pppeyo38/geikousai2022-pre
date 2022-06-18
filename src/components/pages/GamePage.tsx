ef554a37ae48738dad286c8d0beaa9cf0241f289
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-18 13:54:57 +0900

    Unityファイルのテスト配置

--- src/components/pages/GamePage.tsx
+++ src/components/pages/GamePage.tsx
@@ -1,19 +1,19 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export const GamePage = () => {
-	// const { unityProvider } = useUnityContext({
-	// 	loaderUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.loader.js`,
-	// 	dataUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.data`,
-	// 	frameworkUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.framework.js.gz`,
-	// 	codeUrl: `${process.env.PUBLIC_URL}/UnityBuild/build-test.wasm.gz`,
-	// 	webglContextAttributes: {
-  //     preserveDrawingBuffer: true,
-  //   },
-	// });
+	const { unityProvider } = useUnityContext({
+		loaderUrl: `${process.env.PUBLIC_URL}/Build/geikosai616.loader.js`,
+		dataUrl: `${process.env.PUBLIC_URL}/Build/geikosai616.data`,
+		frameworkUrl: `${process.env.PUBLIC_URL}/Build/geikosai616.framework.js`,
+		codeUrl: `${process.env.PUBLIC_URL}/Build/geikosai616.wasm`,
+		// webglContextAttributes: {
+    //   preserveDrawingBuffer: true,
+    // },
+	});
 
 	return (
 		<>
-			{/* <Unity unityProvider={unityProvider} /> */}
+			<Unity unityProvider={unityProvider} />
 			<p>ミニゲーム</p>
 		</>
 	);
