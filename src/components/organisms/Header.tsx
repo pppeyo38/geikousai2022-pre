2251fb0b0adf29dec9b29d36d22b31e37d2c2632
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-20 14:24:36 +0900

    navのaタグをLinkに変更

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -5,19 +5,25 @@ import Hamburger from 'hamburger-react';
 import { SnsIcons } from "../atoms/SnsIcons";
 
 import "../../styles/base/header.scss";
+import { Link } from "react-router-dom";
 
 export const Header = memo(() => {
 	const { isOpen, onToggle } = useDisclosure();
-	const [ booOpen, setOpen ] = useState(false)
+	const [ booOpen, setOpen ] = useState(false);
+
+	const hiddenNav = () => {
+		setOpen(!booOpen);
+		onToggle();
+	}
 
 	return (
 		<>
 			<header>
 				<div className="Header-wrap">
-					<a className="Header-logo-wrap" href="/">
+					<Link className="Header-logo-wrap" to="/">
 						<img className="Header-logo" src={`${process.env.PUBLIC_URL}/icons/geikosai-logo.png`} alt="芸工祭ロゴ画像" width="47px" height="43px" />
 						<img className="Header-title" src={`${process.env.PUBLIC_URL}/icons/HeaderTitle.svg`} alt="芸工祭"/>
-					</a>
+					</Link>
 					<div className="nav__overlay-close-icon-wrap" onClick={onToggle}>
 						<Hamburger
 							size={22}
@@ -26,36 +32,36 @@ export const Header = memo(() => {
 						/>
 					</div>
 					<Fade in={isOpen} className="nav__overlay" style={{ pointerEvents: isOpen ? 'auto': 'none' }}>
-						<ul className="nav__overlay-menuList">
+						<ul className="nav__overlay-menuList" onClick={hiddenNav}>
 							<li>
-								<a href={`${process.env.PUBLIC_URL}/`}>
+								<Link to={`${process.env.PUBLIC_URL}/`}>
 									<h3>TOP</h3>
 									<p>トップ</p>
-								</a>
+								</Link>
 							</li>
 							<li>
-								<a href={`${process.env.PUBLIC_URL}/departments`}>
+								<Link to={`${process.env.PUBLIC_URL}/departments`}>
 									<h3>DEPARTMENTS</h3>
 									<p>部署紹介</p>
-								</a>
+								</Link>
 							</li>
 							<li>
-								<a href={`${process.env.PUBLIC_URL}/chart`}>
+								<Link to={`${process.env.PUBLIC_URL}/chart`}>
 									<h3>CHART</h3>
 									<p>チャート</p>
-								</a>
+								</Link>
 							</li>
 							<li>
-								<a href={`${process.env.PUBLIC_URL}/movie`}>
+								<Link to={`${process.env.PUBLIC_URL}/movie`}>
 									<h3>MOVIE</h3>
 									<p>ムービー</p>
-								</a>
+								</Link>
 							</li>
 							<li>
-								<a href={`${process.env.PUBLIC_URL}/game`}>
+								<Link to={`${process.env.PUBLIC_URL}/game`}>
 									<h3>GAME</h3>
 									<p>ゲーム</p>
-								</a>
+								</Link>
 							</li>
 						</ul>
 						<div className="nav__overlay-sns-wrap">
