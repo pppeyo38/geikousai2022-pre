96767db5d3f14ac4487b68a605fc293109684bb8
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-20 13:50:31 +0900

    SNSアイコンにリンクを設定

--- src/components/atoms/SnsIcons.tsx
+++ src/components/atoms/SnsIcons.tsx
@@ -1,9 +1,15 @@
 export const SnsIcons = () => {
 	return (
 		<div className="footer__sns-wrap">
-			<img src={`${process.env.PUBLIC_URL}/icons/footer/ft_Instagram.svg`} alt="Instagramアイコン" />
-			<img src={`${process.env.PUBLIC_URL}/icons/footer/ft_twitter.svg`} alt="Twitterアイコン" />
-			<img src={`${process.env.PUBLIC_URL}/icons/footer/ft_LINE.svg`} alt="LINEアイコン" />
+			<a href="https://www.instagram.com/geiko.fes_2022/">
+				<img src={`${process.env.PUBLIC_URL}/icons/footer/ft_Instagram.svg`} alt="Instagramアイコン" />
+			</a>
+			<a href="https://twitter.com/geik0" className="">
+				<img src={`${process.env.PUBLIC_URL}/icons/footer/ft_twitter.svg`} alt="Twitterアイコン" />
+			</a>
+			<a href="https://lin.ee/W28H8q6" className="">
+				<img src={`${process.env.PUBLIC_URL}/icons/footer/ft_LINE.svg`} alt="LINEアイコン" />
+			</a>
 		</div>
 	);
 }
