1db2cb7ceea0c2f1b4ce2f0a6bbe7992639a071b
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-06-21 15:48:06 +0900

    スマホからしか見れないようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { isMobile } from "react-device-detect"
 
 import { Router } from "./router/Router";
 
@@ -7,7 +8,21 @@ export default function App() {
   return (
     <ChakraProvider>
       <BrowserRouter basename="/">
-        <Router />
+        {isMobile ? (
+          <Router />
+        ) : (
+          <Flex
+            width="100vw"
+            height="100vh"
+            direction="column"
+            justify="center"
+            align="center"
+            background="linear-gradient(to right, #99C955, #30895E)"
+          >
+            <img src={`${process.env.PUBLIC_URL}/icons/geikosai-logo.png`} alt="芸工祭アイコン" style={{ width: '174px', height: '157px' }} />
+            <p style={{ marginTop: '50px', fontFamily: 'ShinGo', fontSize: '21px', fontWeight: '500', color: 'white' }}>スマートフォンでご覧ください😓</p>
+          </Flex>
+        )}
       </BrowserRouter>
     </ChakraProvider>
   );
