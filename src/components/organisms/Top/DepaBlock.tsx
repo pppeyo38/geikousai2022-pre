e66bcbd3749d3595088de66309e9882acc5f1c51
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-03 17:17:01 +0900

    update 部署紹介ブロック

--- src/components/organisms/Top/DepaBlock.tsx
+++ src/components/organisms/Top/DepaBlock.tsx
@@ -2,6 +2,7 @@ import { Heading } from '@/components/atoms/icons/Heading'
 import { DepaCard } from '@/components/molecules/Department/DepaCrad'
 import styled from '@emotion/styled'
 import Image from 'next/image'
+import { Grid } from '@chakra-ui/react'
 
 export const DepaBlock = () => {
   return (
@@ -9,40 +10,46 @@ export const DepaBlock = () => {
       <_HeadingWrap>
         <Heading>部署紹介</Heading>
       </_HeadingWrap>
-      <_ContentWrap>
-        <_HeadImage>
-          <Image src="/images/departmentHead/saicho.png" layout="fill" alt="" />
-        </_HeadImage>
-        <DepaCard
-          icon="/icons/department/01chief.png"
-          roll="芸工祭長"
-          geikoName="3N 花天狂骨"
-        >
-          芸工祭全体の統括をしてるよ！今年の芸工祭は例年に比べて自分の案が通りやすいかも…？！学年とか気にせずドシドシ意見出してみんなで盛り上げていこう💪いろんな部署にいるから会いにきてね❤️❤️❤️
-        </DepaCard>
-      </_ContentWrap>
-      <_ContentWrap>
-        <_HeadImage>
-          <Image
-            src="/images/departmentHead/fuku-saicho.png"
-            layout="fill"
-            alt=""
-          />
-        </_HeadImage>
-        <DepaCard
-          icon="/icons/department/02chief_sub.png"
-          roll="副芸工祭長"
-          geikoName="30 オクタン"
-        >
-          主に祭長のサポートしてます！インテリア、ディレクションにも入っているので一緒に活動して楽しい芸工祭にしましょう！
-        </DepaCard>
-      </_ContentWrap>
+      <Grid gap="30px">
+        <_ContentWrap>
+          <_HeadImage>
+            <Image
+              src="/images/departmentHead/saicho.png"
+              layout="fill"
+              alt=""
+            />
+          </_HeadImage>
+          <DepaCard
+            icon="/icons/department/01chief.png"
+            roll="芸工祭長"
+            geikoName="3N 花天狂骨"
+          >
+            芸工祭全体の統括をしてるよ！今年の芸工祭は例年に比べて自分の案が通りやすいかも…？！学年とか気にせずドシドシ意見出してみんなで盛り上げていこう💪いろんな部署にいるから会いにきてね❤️❤️❤️
+          </DepaCard>
+        </_ContentWrap>
+        <_ContentWrap>
+          <_HeadImage>
+            <Image
+              src="/images/departmentHead/fuku-saicho.png"
+              layout="fill"
+              alt=""
+            />
+          </_HeadImage>
+          <DepaCard
+            icon="/icons/department/02chief_sub.png"
+            roll="副芸工祭長"
+            geikoName="30 オクタン"
+          >
+            主に祭長のサポートしてます！インテリア、ディレクションにも入っているので一緒に活動して楽しい芸工祭にしましょう！
+          </DepaCard>
+        </_ContentWrap>
+      </Grid>
     </_Section>
   )
 }
 
 const _Section = styled.section`
-  padding: 60px 20px;
+  padding: 60px 20px 0;
 `
 
 const _HeadingWrap = styled.div`
