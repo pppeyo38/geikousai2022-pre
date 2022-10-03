4fea6a1cd83523bb4a8ebcb9befc874314caed22
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-03 19:13:01 +0900

    add game caption

--- src/components/organisms/Top/GameBlock.tsx
+++ src/components/organisms/Top/GameBlock.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 
 import { Heading } from '@/components/atoms/icons/Heading'
 import { ViewMore } from '@/components/atoms/buttons/ViewMore'
+import { Text } from '@/components/atoms/text/Text'
 
 export const GameBlock = () => {
   return (
@@ -15,6 +16,11 @@ export const GameBlock = () => {
           layout="fill"
         />
       </_ThumWrap>
+      <Text fontSize={16} color="#000">
+        スライムを引っ張って遠くまで飛ばそう！
+        <br />
+        たくさん飛ばして目指せランカー！
+      </Text>
       <ViewMore route="/game">ミニゲームをする</ViewMore>
     </_Section>
   )
