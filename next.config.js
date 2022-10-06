bf9fc82c5888cda1749ece12a21983ec29f95ede
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-06 22:44:32 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = '/2022/pre'
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV == 'production'
+
 module.exports = {
+  basePath: isProd ? SUB_DIRECTORY : '',
+  assetPrefix: isProd ? SUB_DIRECTORY : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
